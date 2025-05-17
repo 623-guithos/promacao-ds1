03d6434b3bb2a885612b6b42f3c48814a1b75436
623-guithos <guilherme.estrada.santos@escola.pr.gov.br>
2025-05-16 21:57:49 -0300

    Create calculos.js

---
+++ calculos.js
@@ -0,0 +1,19 @@
+1 console.log("--- Igualdade---");
+3 // Igualdade solta () converte tipos antes de comparar
+console.log(10'10'+${10 '10')", "// Compara valores após conversão (string '10' vira número)");
+6 // Igualdade estrita () compara valores E tipo
+console.log('10 '10'+${10 '10'}", "// Tipos diferentes (number + string)");
+console.log("\n--- Desigualdade ---");
+11 // Desigualdade solta (1) converte tipos antes de comparar
+console.log('51'5' ${5 1= '5')", "// Valores são iguais após conversão");
+14 // Desigualdade estrita (1) compara valores & tipos
+15 console.log('51'5'+$(51= '5'}, "// Tipos diferentes (number string)");
+17 console.log("\n--- Comparações Numéricas ---");
+19 // Maior que
+20 console.log('10 > 5 ${105], "// 10 é maior que 5");
+22 // Menor que
+23 console.log(105${105], "// 10 não é menor que 5")
+25 // Maior ou igual
+26 console.log(10 >= 10${10 >= 10), "// 10 é igual a 10");
+28 // Menor ou igual
+29 console.log(54${5 <= 4)", "// 5 não é menor nem igual a 4");
