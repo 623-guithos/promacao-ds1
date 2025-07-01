99a72797e403e0602ec56095453bb31107d4c919
623-guithos <guilherme.estrada.santos@escola.pr.gov.br>
2025-07-01 11:01:05 -0300

    Create verificaIdade.js

---
+++ verificaIdade.js
@@ -0,0 +1,9 @@
+// Crie uma variável para armazenar a idade da pessoa
+let idade = 17;
+
+// Use uma estrutura condicional SE-SENÃO para verificar a idade
+if (idade >= 18) {
+  console.log("Você é maior de idade.");
+} else {
+  console.log("Você é menor de idade.");
+}
