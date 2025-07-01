acc8ea7ab0074836c222db2f44176315adb22445
623-guithos <guilherme.estrada.santos@escola.pr.gov.br>
2025-07-01 11:05:13 -0300

    Create verificaDia.js

---
+++ verificaDia.js
@@ -0,0 +1,9 @@
+// Crie uma variável para armazenar o nome de um dia da semana
+let dia = "Sábado"; // Você pode mudar para "Domingo", "Segunda-feira", etc.
+
+// Use uma estrutura condicional para verificar se é final de semana
+if (dia === "Sábado" || dia === "Domingo") {
+  console.log("É final de semana!");
+} else {
+  console.log("É dia de semana.");
+}
