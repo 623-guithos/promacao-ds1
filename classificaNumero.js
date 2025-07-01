188d453c6dafb85866c07328d4818ab471d3d52a
623-guithos <guilherme.estrada.santos@escola.pr.gov.br>
2025-07-01 11:10:13 -0300

    Create classificaNumero.js

---
+++ classificaNumero.js
@@ -0,0 +1,12 @@
+// Crie uma variável para armazenar um número inteiro
+let numero = -5; // Experimente mudar para 10, 0, -5, etc.
+
+// Use estruturas condicionais para verificar o número
+if (numero > 0) {
+  console.log("O número é positivo.");
+} else if (numero < 0) {
+  console.log("O número é negativo.");
+} else {
+  // Se não for maior que 0 e nem menor que 0, só pode ser 0
+  console.log("O número é zero.");
+}
