6e4f687c51445b6fc3b1a5f0f0adda6cad7e96c0
623-guithos <guilherme.estrada.santos@escola.pr.gov.br>
2025-07-04 14:57:56 -0300

    Create  resultado-ifelse.js

---
+++ resultado-ifelse.js
@@ -0,0 +1,7 @@
+let avaliacao = 8.5; // Exemplo de nota com casa decimal (substitua pelo valor desejado)
+
+if (avaliacao > 7.0) { // 7.0 é a nota de aprovação
+    console.log("Você está aprovado. Parabéns!"); 
+} else { 
+    console.log("Infelizmente você não atingiu a nota mínima."); 
+}
